2a1d1d3c770b2cc99b6f7ed82e98d6f5ff2ae848
RodriSebastian <rodrigoriquelme2198@gmail.com>
2024-12-02 21:10:35 -0300

    Add user registration, login and token generation

---
+++ src/routes/authRouthes.js
@@ -0,0 +1,22 @@
+import {Router} from "express";
+import {
+    login, 
+    register, 
+    logout, 
+    profile,
+} from "../controllers/authController.js";
+import {authRequired} from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validatorMiddleware.js";
+import { registerSchema, loginSchema } from "../schemas/authSchema.js";
+
+const router = Router();
+
+router.post("/register", validateSchema(registerSchema), register);
+
+router.post("/login", validateSchema(loginSchema), login);
+
+router.post("/logout", logout);
+
+router.get("/profile", authRequired, profile);
+
+export default router

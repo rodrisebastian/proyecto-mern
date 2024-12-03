5b744098350e265d6230d4cf720f9f2813d2efe8
RodriSebastian <rodrigoriquelme2198@gmail.com>
2024-12-02 21:25:58 -0300

    Add task-crud and validation token

---
+++ src/routes/taskRouthes.js
@@ -0,0 +1,26 @@
+import {Router} from 'express';
+import { authRequired } from '../middlewares/validateToken';
+import {
+    createTask,
+    deleteTask,
+    getTask,
+    getTasks,
+    updateTask,
+  } from "../controllers/tasksControllers.js";
+  import { auth } from "../middlewares/authMiddleware.js";
+  import { validateSchema } from "../middlewares/validatorMiddleware.js";
+  import { createTaskSchema } from "../schemas/taskSchema.js";
+  
+  const router = Router();
+  
+  router.get("/tasks", authRequired, getTasks);
+  
+  router.post("/tasks", authRequired, validateSchema(createTaskSchema), createTask);
+  
+  router.get("/tasks/:id", authRequired, getTask);
+  
+  router.put("/tasks/:id", authRequired, updateTask);
+  
+  router.delete("/tasks/:id", authRequired, deleteTask);
+  
+  export default router;

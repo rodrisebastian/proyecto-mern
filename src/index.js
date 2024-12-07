6a92ab256ccc5570a078b2915cb169c0f0aa2767
RodriSebastian <rodrigoriquelme2198@gmail.com>
2024-12-07 00:54:31 -0300

    refactor: index and .env improvement

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
 import dotenv from 'dotenv';
+dotenv.config();
 
 async function main() {
   try {
     await connectDB();
     app.listen(PORT);
-    dotenv.config();
     console.log(`Listening on port http://localhost:${PORT}`);
     console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {

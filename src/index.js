d67250027d81f4a5153796b99589eda258fe2dd9
RodriSebastian <rodrigoriquelme2198@gmail.com>
2024-12-07 00:34:48 -0300

    creation of .env file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
+import dotenv from 'dotenv';
 
 async function main() {
   try {
     await connectDB();
     app.listen(PORT);
+    dotenv.config();
     console.log(`Listening on port http://localhost:${PORT}`);
     console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {
